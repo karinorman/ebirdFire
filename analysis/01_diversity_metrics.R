2b1c5628ad524cad5b7d09d84cd54f19736ca3ff
karinorman <kari@normanfamily.org>
2024-07-16 13:21:06 -0600

    write out community matrices, debug

--- analysis/01_diversity_metrics.R
+++ analysis/01_diversity_metrics.R
@@ -2,6 +2,9 @@ library(dplyr)
 library(terra)
 library(purrr)
 library(tidyr)
+library(tidyterra)
+
+boundary_shp <- vect(here::here("data/study_boundary.shp"))
 
 # residents
 resident_path <- here::here("data/species_ranges_wus/resident")
@@ -12,12 +15,10 @@ breeding_path <- here::here("data/species_ranges_wus/breeding")
 breeding_files <- paste0(breeding_path, "/", list.files(breeding_path))
 
 # nonbreeding
-nonbreeding_path <- here::here("data/species_ranges_wus/nonbreeding")
+nonbreeding_path <- here::here("data/species_ranges_wus/non_breeding")
 nonbreeding_files <- paste0(nonbreeding_path, "/", list.files(nonbreeding_path))
 
 raster_to_df <- function(raster_path){
-
-  browser()
 
   raster <- rast(raster_path)
   species <- names(raster)
@@ -34,41 +35,65 @@ raster_to_df <- function(raster_path){
 
 # Get list of species rasters as df, remove empty df (species don't occur in the extent)
 breeding_df_list <- c(resident_files, breeding_files) %>%
-  map(., raster_to_df) %>% compact()
+  map(., raster_to_df) %>% plyr::compact()
+
+# # check the df dimensions of each shapefile
+# c(resident_files, breeding_files) %>%
+#   map(., ~terra::as.data.frame(rast(.x)) %>% dim() %>% .[1])
 
 nonbreeding_df_list <- c(resident_files, nonbreeding_files) %>%
   map(., raster_to_df) %>% compact()
 
 # join by the cell
-breeding_df <- plyr::join_all(breeding_df_list)
-nonbreeding_df <- plyr::join_all(nonbreeding_df_list)
+breeding_df <- plyr::join_all(breeding_df_list, type = "full")
+nonbreeding_df <- plyr::join_all(nonbreeding_df_list, type = "full")
 
 usethis::use_data(breeding_df)
 usethis::use_data(nonbreeding_df)
 
-
-comm_mat <- breeding_df %>%
+breeding_mat <- breeding_df %>%
   select(-c(cell, x, y)) %>%
   mutate(across(everything(), ~tidyr::replace_na(., 0)))
 
-breeding_beta <- adespatial::beta.div(comm_mat, samp = FALSE, nperm = 10)
+nonbreeding_mat <- nonbreeding_df %>%
+  select(-c(cell, x, y)) %>%
+  mutate(across(everything(), ~tidyr::replace_na(., 0)))
+
+write.csv(breeding_mat, here::here("data/breeding_occ_mat.csv"))
+write.csv(nonbreeding_mat, here::here("data/nonbreeding_occ_mat.csv"))
+
+breeding_beta <- adespatial::beta.div(comm_mat, samp = FALSE, nperm = 999)
 usethis::use_data(breeding_beta)
 
 # get LCBD as raster
 # read in raster to use as template
-temp_rast <- c(rast(breeding_files[1]), rast(breeding_files[2]),
-               rast(breeding_files[3]))
+temp_rast <- c(rast(breeding_files[1]), rast(breeding_files[1]),
+               rast(breeding_files[1]))
 temp_rast_df <- terra::as.data.frame(temp_rast, cells = TRUE, xy = TRUE)
 
 breeding_lcbd <- tibble(LCBD = breeding_beta$LCBD,
                         p.LCBD = breeding_beta$p.LCBD,
                         p.adj = breeding_beta$p.adj) %>%
-  bind_cols(breeding_df %>% select(cell, x, y), .) %>%
-  left_join(temp_rast_df %>% select(cell, x, y), .)
+  bind_cols(breeding_df %>% select(x, y), .) #%>%
+  # bind_cols(breeding_df %>% select(cell, x, y), .) %>%
+  # left_join(temp_rast_df %>% select(cell, x, y), .) %>%
+  # select(-c())
 
-lcbd_rast <- setValues(temp_rast, breeding_lcbd)
+breeding_lcbd_rast <- rast(breeding_lcbd, type="xyz", crs = "epsg:4326")
 
 
+### let's figure out what the heck is going on
+richness_rast <- breeding_df %>%
+  mutate(sum = rowSums(across(-c(cell, x, y)), na.rm = TRUE)) %>%
+  select(x, y, sum) %>%
+  rast(type = "xyz")
+
+ggplot() +
+  geom_spatvector(data = boundary_shp) +
+  geom_spatraster(data = richness_rast)
+#lcbd_rast <- setValues(temp_rast, breeding_lcbd)
+
+temp_rast[breeding_lcbd$cell] <- breeding_lcbd[,-1]
 
 #
 # ##### Get single raster for breeding + residents #####
