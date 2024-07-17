6100d70002990080d888db1ea7a3ec78d8299fab
karinorman <kari@normanfamily.org>
2024-07-17 14:06:26 -0600

    comments, save out

--- analysis/01_lcbd_rasters.R
+++ analysis/01_lcbd_rasters.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(terra)
 library(tidyterra)
 
+# read in csv's of lcbd calculated by julia script
 breeding_lcbd <- read.csv(here::here("data/breeding_lcbd.csv")) %>%
   rename(breeding_lcbd = LCBD)
 nonbreeding_lcbd <- read.csv(here::here("data/nonbreeding_lcbd.csv")) %>%
@@ -9,10 +10,13 @@ nonbreeding_lcbd <- read.csv(here::here("data/nonbreeding_lcbd.csv")) %>%
 
 coords <- read.csv(here::here("data/raster_coords.csv"))
 
-
+# combine breeding and nonbreeding lcbd, add coordinates
 lcbd <- full_join(breeding_lcbd, nonbreeding_lcbd) %>%
   full_join(coords) %>%
   select(x, y, breeding_lcbd, nonbreeding_lcbd)
 
-
+# transform to raster
 lcbd_rast <- rast(lcbd, type="xyz", crs = "epsg:4326")
+
+#save out
+writeRaster(lcbd_rast, here::here("data/lcbd_rast.tiff"))
