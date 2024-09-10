6218d9e5145a3f50d765b9af3457b3c7c2da8a7f
karinorman <kari@normanfamily.org>
2024-09-10 16:03:23 -0600

    Integrate the new master database of range metrics, do contrasts.

--- analysis/modeling.R
+++ analysis/modeling.R
@@ -5,33 +5,35 @@ library(betareg)
 library(broom)
 
 load(here::here("data/avonet_ebird_matched.rda"))
-load(here::here("data/percent_range_highsev_df.rda"))
+load(here::here("data/species_range_metrics.rda"))
 
-load(here::here("data/range_area_df.rda"))
-
-species_list <- range_area_df %>%
+traits_df <- species_range_metrics %>%
   filter(wus_percent > 0.1) %>%
-  pull(species_code)
-
-traits_df <- percent_range_highsev_df %>%
   left_join(avonet_ebird_matched) %>%
   select(-c(scientific.name, taxon_concept_id, avonet_sciname,
-            avonet_taxon_concept_id, avonet_family, avonet_order)) %>%
-  filter(species_code %in% species_list)
+            avonet_taxon_concept_id, avonet_family, avonet_order,
+            total_range_area, wus_range_area, wus_percent, range_type))
 
 predictors <- colnames(traits_df)[!colnames(traits_df) %in% c("species_code", "resident_percent",
-                                                              "breeding_percent", "nonbreeding_percent")]
+                                                              "breeding_percent", "nonbreeding_percent", "Mass")] %>%
+  c("log(Mass)",.)
 
-fit <- glm(formula = reformulate(termlabels = predictors, response = "resident_percent"), data = traits_df)
-summary(fit)
+#beta_fit <- betareg(reformulate(termlabels = predictors[!predictors %in% c("Trophic.Niche")], response = "high_sev_percent"), data = traits_df, link = "logit")
 
-# fit <- glm(formula = "breeding_percent ~ Mass", data = traits_df)
-# summary(fit)
-
-beta_fit <- betareg(reformulate(termlabels = predictors, response = "resident_percent"), data = traits_df, link = "logit")
-
-morphology_fit <- betareg(resident_percent ~ Beak.Length_Culmen + Beak.Length_Nares + Beak.Width + Beak.Depth + Tarsus.Length+ Wing.Length +
-                            Kipps.Distance + Secondary1 + Hand.Wing.Index + Tail.Length + Mass,
+morphology_fit <- betareg(high_sev_percent ~ Beak.Length_Culmen + Beak.Depth + Tarsus.Length+
+                            Kipps.Distance + Secondary1 + Hand.Wing.Index + Tail.Length + log(Mass),
                           data = traits_df, link = "logit")
 
-habitat_fit <- betareg(resident_percent ~ Habitat + Habitat.Density, data = traits_df, link = "logit")
+mass_fit <- betareg(high_sev_percent ~ log(Mass), data = traits_df, link = "logit")
+
+habitat_fit <- betareg(high_sev_percent ~ Habitat.Density + Habitat, data = traits_df, link = "logit")
+habitat_contrasts <- emmeans(habitat_fit, pairwise ~ Habitat, data = traits_df)$contrasts %>% as.data.frame()
+
+
+#lifestyle_fit <- betareg(high_sev_percent ~ as.factor(Migration) + Trophic.Niche + Primary.Lifestyle, data = traits_df, link = "logit")
+lifestyle_fit <- betareg(high_sev_percent ~ Migration + Trophic.Niche + Primary.Lifestyle, data = traits_df, link = "logit")
+trophic_niche_contrasts <- emmeans(lifestyle_fit, pairwise ~ Trophic.Niche, data = traits_df)$contrasts %>% as.data.frame()
+lifestyle_contrasts <- emmeans(lifestyle_fit, pairwise ~ Primary.Lifestyle, data = traits_df)$contrasts %>% as.data.frame()
+#migration_contrasts <- emmeans(lifestyle_fit, pairwise ~ as.factor(Migration), data = traits_df)$contrasts %>% as.data.frame()
+
+
