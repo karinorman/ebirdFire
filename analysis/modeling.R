d335f7ea85502fed7c4eba2445545852ef408345
karinorman <kari@normanfamily.org>
2024-12-02 11:10:21 -0700

    Fit models and out put as a dataframe, still need to fix contrasts

--- analysis/modeling.R
+++ analysis/modeling.R
@@ -7,34 +7,51 @@ library(emmeans)
 
 load(here::here("data/avonet_ebird_matched.rda"))
 load(here::here("data/species_range_metrics.rda"))
+load(here::here("data/pop_mets.rda"))
 
 traits_df <- species_range_metrics %>%
   filter(wus_percent > 0.1) %>%
   left_join(avonet_ebird_matched) %>%
   select(-c(scientific.name, taxon_concept_id, avonet_sciname,
             avonet_taxon_concept_id, avonet_family, avonet_order,
-            total_range_area, wus_range_area, wus_percent, range_type))
+            total_area, wus_area, wus_percent, sev_area)) %>%
+  left_join(pop_mets %>% select(species_code, pop_percent = percent)) %>%
+  rename(range = high_sev_percent, population = pop_percent) %>%
+  pivot_longer(cols = c(range, population), names_to = "percent_type", values_to = "percent")
 
-predictors <- colnames(traits_df)[!colnames(traits_df) %in% c("species_code", "resident_percent",
-                                                              "breeding_percent", "nonbreeding_percent", "Mass")] %>%
-  c("log(Mass)",.)
 
-#beta_fit <- betareg(reformulate(termlabels = predictors[!predictors %in% c("Trophic.Niche")], response = "high_sev_percent"), data = traits_df, link = "logit")
+fit_beta <- function(percent_type, model_id, formula){
 
-morphology_fit <- betareg(high_sev_percent ~ Beak.Length_Culmen + Beak.Depth + Tarsus.Length+
-                            Kipps.Distance + Secondary1 + Hand.Wing.Index + Tail.Length + log(Mass),
-                          data = traits_df, link = "logit")
+  fit <- betareg(formula = formula,
+                 data = traits_df %>% filter(percent_type == !!percent_type), link = "logit")
 
-#mass_fit <- betareg(high_sev_percent ~ log(Mass), data = traits_df, link = "logit")
+  df <- broom::tidy(fit) %>%
+    mutate(percent_type = percent_type,
+           model_id = model_id)
 
-habitat_fit <- betareg(high_sev_percent ~ Habitat.Density + Habitat, data = traits_df, link = "logit")
+  return(list(df, fit))
+}
+
+
+model_df <- data.frame(percent_type = rep(c("range", "population"), 3),
+                       model_id = rep(c("morphology", "habitat", "llifestyle"), each = 2),
+                       formula = rep(c("percent ~ Beak.Length_Culmen + Beak.Depth + Tarsus.Length + Kipps.Distance + Secondary1 + Hand.Wing.Index + Tail.Length + log(Mass)",
+                                       "percent ~ Habitat.Density + Habitat",
+                                       "percent ~ Migration + Trophic.Niche + Primary.Lifestyle"), each = 2))
+
+model_output <- purrr::pmap(model_df, fit_beta)
+
+model_output_df <- map(model_output, 1) %>% bind_rows()
+model_fits <- map(model_output_df, 2)
+
+model_output_df %>% filter(p.value < 0.05, term != "(Intercept)", component == "mu") %>% View()
+
+
+
 habitat_contrasts <- emmeans(habitat_fit, pairwise ~ Habitat, data = traits_df)$contrasts %>% as.data.frame()
 
 
-#lifestyle_fit <- betareg(high_sev_percent ~ as.factor(Migration) + Trophic.Niche + Primary.Lifestyle, data = traits_df, link = "logit")
-lifestyle_fit <- betareg(high_sev_percent ~ Migration + Trophic.Niche + Primary.Lifestyle, data = traits_df, link = "logit")
 trophic_niche_contrasts <- emmeans(lifestyle_fit, pairwise ~ Trophic.Niche, data = traits_df)$contrasts %>% as.data.frame()
 lifestyle_contrasts <- emmeans(lifestyle_fit, pairwise ~ Primary.Lifestyle, data = traits_df)$contrasts %>% as.data.frame()
-#migration_contrasts <- emmeans(lifestyle_fit, pairwise ~ as.factor(Migration), data = traits_df)$contrasts %>% as.data.frame()
 
 
