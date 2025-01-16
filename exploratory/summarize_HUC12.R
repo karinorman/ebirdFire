170c8c71b5211c8a61674ffa342ee6dad26c1756
karinorman <kari@normanfamily.org>
2025-01-16 15:27:38 -0700

    We've got a plot! With the naive verison of id'ing hotspot type (i.e. modal fire severity)

--- exploratory/summarize_HUC12.R
+++ exploratory/summarize_HUC12.R
@@ -19,6 +19,14 @@ ecoregion_shp <- vect(here::here("raw_data/ecoregions/ecoregions_edc.shp")) %>%
   project("epsg:4326") %>%
   crop(., boundary)
 
+ecoregion_rast <- ecoregion_shp %>%
+  select("ECO_NAME") %>%
+  terra::rasterize(., cbi, field = "ECO_NAME")
+
+# get ecoregion labels for each HUC12
+ecoregion_labels <- zonal(ecoregion_rast, HUC12, fun = "modal", na.rm = TRUE) %>%
+  cbind(huc12 = HUC12$huc12, .)
+
 #huc12_ecoregion <- intersect(HUC12 %>% select(huc12), ecoregion_shp %>% select(ECO_NAME, ECO_NUM))
 
 # mtbs <- vect(here::here("raw_data/mtbs_perimeter_data/mtbs_perims_DD.shp")) %>%
@@ -32,9 +40,14 @@ ecoregion_shp <- vect(here::here("raw_data/ecoregions/ecoregions_edc.shp")) %>%
 #   geom_spatraster(data = cbi_mask) +
 #   geom_spatvector(data = mtbs)
 
+# get frequency of CBI values within polygons to figure out what kind of hotspot they are
+huc12_cbi_modal_vect <- zonal(cbi, HUC12, fun = "modal", na.rm = TRUE, as.polygons = TRUE)
+
 # get HUC12 level means of biodiversity metrics
 biodiv_zonal <- zonal(biodiv_rast, HUC12, fun = "mean", na.rm = TRUE) %>%
-  cbind(huc12 = HUC12$huc12, .)
+  cbind(huc12 = HUC12$huc12, .) %>%
+  left_join(ecoregion_labels) %>%
+  left_join(values(huc12_cbi_modal_vect) %>% select(huc12, fire = predict.high.severity.fire.final))
 
 # rejoin with polygon labels and geometry
 biodiv_zonal_vec <- HUC12 %>% select(huc12) %>%
@@ -42,12 +55,20 @@ biodiv_zonal_vec <- HUC12 %>% select(huc12) %>%
 
 # get spatvector of hotspots for each metric
 hotspot_zonal_vec <- biodiv_zonal_vec %>%
-  mutate(across(-c(huc12),
+  # get hotspots only in forested areas
+  filter(fire %in% c(1,2)) %>%
+  mutate(across(-c(huc12, ECO_NAME, fire),
                 ~ifelse(.x > quantile(.x, probs = 0.95, na.rm = TRUE), 1, NA)))
 
-# get frequency of CBI values within polygons to figure out what kind of hotspot they are
-huc12_cbi_modal_vect <- zonal(cbi, HUC12, fun = "modal", na.rm = TRUE, as.polygons = TRUE)
+ecoregion_hotspot_zonal_vec <- HUC12 %>% select(huc12) %>%
+  left_join(biodiv_zonal %>%
+  filter(!is.na(ECO_NAME), fire %in% c(1,2)) %>%
+  group_by(ECO_NAME) %>%
+  mutate(across(-c(huc12, fire),
+                ~ifelse(.x > quantile(.x, probs = 0.95, na.rm = TRUE), 1, NA))) %>%
+  ungroup())
 
+# get the break down of CBI types within each huc12 to classify what kind of hotspot it is
 huc_cbi_perc <- extract(cbi, HUC12) %>%
   select(ID, predict.high.severity.fire.final) %>%
   count(ID, predict.high.severity.fire.final) %>%
@@ -66,12 +87,22 @@ huc_cbi_perc <- extract(cbi, HUC12) %>%
 
 biodiv_cbi_huc12 <- biodiv_zonal_vec %>% filter(huc12 %in% huc_cbi_perc$huc12) %>% left_join(huc_cbi_perc)
 
-test_plot <- ggplot() +
-  geom_spatvector(data = biodiv_zonal_vec, aes(fill = breeding_richness), color = "transparent")
+# test_plot <- ggplot() +
+#   geom_spatvector(data = biodiv_zonal_vec, aes(fill = breeding_richness, color = breeding_richness))
+#
+# ggsave(here::here("figures/test_map.jpeg"), test_plot)
+#
+# hotspot_plot <- ggplot() +
+#   geom_spatvector(data = hotspot_zonal_vec, aes(fill = breeding_richness, color = breeding_richness))
+#
+# ggsave(here::here("figures/huc12_hotspot.jpeg"), hotspot_plot)
 
-ggsave(here::here("figures/test_map.jpeg"), test_plot)
+ecoregion_hotspot_plot <- ggplot() +
+  geom_spatvector(data = boundary, color = "black", fill = "white") +
+  geom_spatvector(data = ecoregion_hotspot_zonal_vec %>% filter(breeding_richness == 1) %>% mutate(fire = as.factor(fire)),
+                  aes(fill = fire, color = fire)) +
+  theme_void() +
+  scale_fill_manual(values = list(`1` = "#82A6B1", `2` =  "#BC4749"), na.value = "transparent") +
+  scale_color_manual(values = list(`1` = "#82A6B1", `2` =  "#BC4749"), na.value = "transparent")
 
-hotspot_plot <- ggplot() +
-  geom_spatvector(data = hotspot_zonal_vec, aes(fill = breeding_richness), color = "transparent")
-
-ggsave(here::here("figures/huc12_hotspot.jpeg"), hotspot_plot)
+ggsave(here::here("figures/huc12_ecoregion_hotspot.jpeg"), ecoregion_hotspot_plot)
