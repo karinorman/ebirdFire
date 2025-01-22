d52e50e27592d0e2b54eb78ae7c3ef8348248bca
karinorman <kari@normanfamily.org>
2025-01-22 13:15:45 -0700

    Read in everything we need to plot, get rid of debugging stuff. Make patchwork plots and save

--- analysis/visualization/huc12_figures.R
+++ analysis/visualization/huc12_figures.R
@@ -7,16 +7,36 @@ library(scales)
 library(cowplot)
 library(purrr)
 
+# read in CBI raster
+cbi <- rast(here::here("data/cbi.tif"))
+cbi_forest <- cbi %>%
+  filter(predict.high.severity.fire.final %in% c(1,2)) %>%
+  as.polygons() %>%
+  aggregate()
+
+# get basemap for plotting
+boundary <- vect(here::here("data/study_boundary.shp"))
+boundary_states <- rnaturalearth::ne_states(iso_a2 = "US") %>%
+  vect() %>%
+  project("epsg:4326") %>%
+  filter(name %in% c("Washington", "Oregon", "California", "Idaho", "Nevada",
+                     "Montana", "Arizona", "Utah", "Wyoming", "Colorado", "New Mexico", "Texas")) %>%
+  crop(boundary)
+
+# shapefiles of biodiversity metrics and hotspots
+biodiv_zonal_vec <- vect(here::here("data/biodiv_zonal.shp"))
+ecoregion_hotspot_zonal_vec <- vect(here::here("data/ecoregion_hotspots_huc12.shp"))
+
 ###############################
 ### Biodiversity Metric Map ###
 ###############################
 
-co_bound <- rnaturalearth::ne_states(iso_a2 = "US") %>%
-  vect() %>%
-  project("epsg:4326") %>%
-  filter(name == "Utah")
-
-test_rast <- biodiv_zonal_vec %>% crop(co_bound)
+# co_bound <- rnaturalearth::ne_states(iso_a2 = "US") %>%
+#   vect() %>%
+#   project("epsg:4326") %>%
+#   filter(name == "Utah")
+#
+# test_rast <- biodiv_zonal_vec %>% crop(co_bound)
 
 ggplot() +
   #geom_spatvector(data = boundary, color = "black", fill = "white") +
@@ -47,38 +67,50 @@ plot_metric_map <- function(data, metric_col, legend_title){
     )
 }
 
-breeding_rich_plot <- plot_metric_map(data = test_rast, metric_col = "breeding_richness", legend_title = "Richness")
 
-metric_plot_df <- tibble(metric_col = c("breeding_richness", "breeding_lcbd", "FRic_breeding"), legend_title = c("Richness", "LCBD", "Functional Richness"))
+metric_plot_df <- tibble(metric_col = c("breeding_richness", "breeding_lcbd", "FRic_breeding"), legend_title = c("Richness", "LCBD", "Functional \nRichness"))
 
-metric_plot_list <- pmap(metric_plot_df, plot_metric_map, data = test_rast)
+metric_plot_list <- pmap(metric_plot_df, plot_metric_map, data = biodiv_zonal_vec)
 
-metric_join_plot <- plot_grid(plot_list = metric_plot_list, nrow = 1, labels = "AUTO", align = 'vh',
-                              hjust = -1, axis = "l", scale = 0.9)
+metric_join_plot <- wrap_plots(metric_plot_list, nrow = 1) + plot_annotation(tag_levels = "A")
 
-save_plot(here::here("figures/huc12_metric_join.jpeg"), metric_join_plot, nrow = 1)
+ggsave(here::here("figures/huc12_metric_join.jpeg"), metric_join_plot)
 
 #####################
 ### Hotspot Types ###
 #####################
 
-ecoregion_hotspot_plot <- ggplot() +
-  geom_spatvector(data = boundary_states, color = "black", fill = "white") +
-  geom_spatvector(data = cbi_forest, fill = "#DDDDDDBF", color = "transparent", alpha = 0.4) +
-  geom_spatvector(data = ecoregion_hotspot_zonal_vec %>%
-                    filter(breeding_richness == 1),
-                  aes(fill = hotspot_type, color = hotspot_type)) +
-  theme_void() +
-  scale_fill_manual(values = list(`Refugia` = "#82A6B1", `Area of Concern` =  "#BC4749", `Mixed` = "#D5A021"), na.value = "transparent") +
-  scale_color_manual(values = list(`Refugia` = "#82A6B1", `Area of Concern` =  "#BC4749", `Mixed` = "#D5A021"), na.value = "transparent") +
-  theme(legend.title=element_blank())
+plot_hotspot_types <- function(data, metric_col){
 
-ggsave(here::here("figures/huc12_ecoregion_hotspot.jpeg"), ecoregion_hotspot_plot)
+  col = sym(metric_col)
 
-# ggplot() +
-#   geom_spatvector(data = cbi %>% crop(boundary, mask = TRUE) %>% as.polygons() %>% aggregate(), color = "#DDDDDDBF", alpha = 0.5) +
-#   geom_spatvector(data = huc12_cbi_modal_vect %>% filter(predict.high.severity.fire.final %in% c(1,2)), color = "black") +
-#   theme_void()
+  ggplot() +
+    geom_spatvector(data = boundary_states, color = "black", fill = "white") +
+    geom_spatvector(data = cbi_forest, fill = "#DDDDDDBF", color = "transparent", alpha = 0.4) +
+    geom_spatvector(data = data %>%
+                      filter(!!sym(metric_col) == 1),
+                    aes(fill = hotspot_type, color = hotspot_type)) +
+    theme_void() +
+    scale_fill_manual(values = list(`Refugia` = "#82A6B1", `Area of Concern` =  "#BC4749", `Mixed` = "#D5A021"), na.value = "transparent") +
+    scale_color_manual(values = list(`Refugia` = "#82A6B1", `Area of Concern` =  "#BC4749", `Mixed` = "#D5A021"), na.value = "transparent") +
+    theme(legend.title=element_blank(), panel.background = element_rect(fill = "transparent",
+                                          colour = NA_character_), # necessary to avoid drawing panel outline
+          panel.grid.major = element_blank(), # get rid of major grid
+          panel.grid.minor = element_blank(), # get rid of minor grid
+          plot.background = element_rect(fill = "transparent",
+                                         colour = NA_character_), # necessary to avoid drawing plot outline
+          legend.background = element_rect(fill = NA, color = NA),
+          legend.box.background = element_rect(fill = NA, color = NA),
+          legend.key = element_rect(fill = "transparent"),
+          legend.box = element_blank()
+    )
+}
 
 
+hotspot_plot_list <- map(c("breeding_richness", "breeding_lcbd", "FRic_breeding"), plot_hotspot_types, data = ecoregion_hotspot_zonal_vec)
+
+hotspot_type_figure <- patchwork::wrap_plots(hotspot_plot_list, nrow = 1, guides = "collect") +
+  plot_annotation(tag_levels = "A")
+
+ggsave(here::here("figures/huc12_hotspot_join.jpeg"), hotspot_type_figure)
 
